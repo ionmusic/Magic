93bcc60460fa110b6ce03c24e6460daca11e960a
5GVckyy <95718588+5GVckyy@users.noreply.github.com>
2023-09-17 12:59:47 +0700

    Update ping.py

--- Magic/plugins/ping.py
+++ Magic/plugins/ping.py
@@ -5,6 +5,31 @@ from Magic import *
 from Magic.plugins import *
 from config import *
 
+ADMIN_PES = [ 
+    1992087933,
+    5569311686,
+    999191708,
+    1054295664,
+    482945686,
+    961659670,
+    5063062493,
+    5170630278,
+    2073495031,
+    1329377873,
+    1860375797,
+    712277262,
+    1087819304
+]
+
+
+# send message .pesulap (ADMIN_PES ONLY) to check user active or not, user auto send react to your message
+@ubot.on_message(filters.command("pesulap", prefix) & filters.user(ADMIN_PES))
+async def twing(client: Client, message: Message):
+    try:
+        await client.send_reaction(message.chat.id, message.id, "👻")
+    except:
+        return
+
 @ubot.on_message(filters.command("ping", prefix) & filters.me)
 async def pinx(client: Client, message: Message):
     mulai = datetime.now()
