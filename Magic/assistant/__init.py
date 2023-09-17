51cf964b092cd6ea8e8c46ddf5f94996254a796d
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-09-17 17:02:45 +0700

    update __init.py

--- Magic/assistant/__init.py
+++ Magic/assistant/__init.py
@@ -1,1 +1,34 @@
 
+from config import LOAD, NOLOAD
+
+def __list_all_modules():
+    from os.path import dirname, basename, isfile
+    import glob
+
+    mod_paths = glob.glob(dirname(__file__) + "/*.py")
+    all_modules = [basename(f)[:-3] for f in mod_paths if isfile(f)
+                   and f.endswith(".py")
+                   and not f.endswith('__init__.py')]
+
+    if BOT_LOAD or BOT_NOLOAD:
+        to_load = BOT_LOAD
+        if to_load:
+            if not all(any(mod == module_name for module_name in all_modules) for mod in to_load):
+                print("Tidak dapat mengimport plugis")
+                quit(1)
+
+        else:
+            to_load = all_modules
+
+        if BOT_NOLOAD:
+            print("Plugins tidak terpasang{}".format(BOT_NOLOAD))
+            return [item for item in to_load if item not in BOT_NOLOAD]
+
+        return to_load
+
+    return all_modules
+
+
+ALL_MODULES = sorted(__list_all_modules())
+print("Plugins Bot Terinstall : %s", str(ALL_MODULES))
+__all__ = ALL_MODULES + ["ALL_MODULES"]
