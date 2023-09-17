2b52f305694f0cf76410c2c2adefd504692e67b2
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-09-17 15:39:41 +0700

    update __main__.py

--- Magic/__main__.py
+++ Magic/__main__.py
@@ -16,26 +16,26 @@ async def done():
 
 async def main():
     try:
-        LOGGER("info").info(f"Koneksi Ke Database {MDB.name}...")
+        print(f"Koneksi Ke Database {MDB.name}...")
         if MDB.ping():
-            LOGGER("info").info(f"Koneksi Berhasil Ke {MDB.name}..")
+            print(f"Koneksi Berhasil Ke {MDB.name}..")
         if bot:
             await bot.start()
             bot_id = (await bot.get_me()).username
-            LOGGER("info").info(f"Bot ID: {bot_id} Berhasil Diaktifkan.")
+            print(f"Bot ID: {bot_id} Berhasil Diaktifkan.")
             
         if ubot:
             await ubot.start()
             client_id = (await ubot.get_me()).id
             MDB.set_key("OWNER_ID", ubot.me.id)
-            LOGGER("info").info(f"Client ID: {client_id} Berhasil Diaktifkan")
+            print(f"Client ID: {client_id} Berhasil Diaktifkan")
         
         await loadPlugins()
         await done()
         await idle()
         await aiosession.close()
     except Exception as e:
-        LOGGER("error").error(f"Error: {e}")
+        print(f"Error: {e}")
 
 if __name__ == "__main__":
     install()
