d23857220b38a6c972dfee9b17af98dd92b0480f
IONBOT <113283570+ionmusic@users.noreply.github.com>
2023-09-17 21:51:57 +0700

    Update utils.py

--- Magic/helpers/utils.py
+++ Magic/helpers/utils.py
@@ -34,3 +34,30 @@ async def edit_or_reply(message: Message, *args, **kwargs) -> Message:
         else (message.reply_to_message or message).reply_text
     )
     return await anu(*args, **kwargs)
+
+async def extract_userid(message, text: str):
+    def is_int(text: str):
+        try:
+            int(text)
+        except ValueError:
+            return False
+        return True
+
+    kontol = text.strip()
+
+    if is_int(kontol):
+        return int(kontol)
+
+    meledak = message.entities
+    kocok = message._client
+    if len(entities) < 2:
+        return (await kocok.get_users(kontol)).id
+    mmk = meledak[1]
+    if mmk.type == "mention":
+        return (await kocok.get_users(kontol)).id
+    if mmk.type == "text_mention":
+        return mmk.user.id
+    return None
+
+async def extract_user(message):
+    return (await extract_user_and_reason(message))[0]
