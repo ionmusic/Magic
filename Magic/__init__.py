f89422b313d06edcdbdf30a8ed2535bde12c426b
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-09-17 16:37:06 +0700

    update __init__.py

--- Magic/__init__.py
+++ Magic/__init__.py
@@ -1,28 +1,15 @@
-import logging
-import sys
+
 import asyncio
 
 from aiohttp import ClientSession
-
-from config import *
-from pyrogram import *
+from Magic.helpers._load import *
 
 aiosession = ClientSession()
 
 loop = asyncio.get_event_loop_policy()
 event_loop = loop.get_event_loop()
 
-bot = Client(
-  name="bot",
-  api_id=API_ID,
-  api_hash=API_HASH,
-  bot_token=BOT_TOKEN,
-)
 
-ubot = Client(
-  name="ubot",
-  api_id=API_ID,
-  api_hash=API_HASH,
-  session_string=SESSION,
-  device_model="MagicProject",
-)
+bot = MPBot()
+
+ubot = MPUser()
