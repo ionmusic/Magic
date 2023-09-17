ec8fb30bb4031407358b013a6f7ce609c62dc907
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-09-17 17:17:38 +0700

    update __main__.py

--- Magic/__main__.py
+++ Magic/__main__.py
@@ -1,10 +1,13 @@
 import asyncio
+import importlib
 
 from pyrogram import idle
 from Magic import *
 from Magic.helpers import *
 from Magic.helpers._database import *
-from Magic.helpers._load import *
+from Magic.assistant import ALL_SETTINGS
+from Magic.plugins import ALL_MODULES
+
 from uvloop import install
 
 async def done():
@@ -15,22 +18,57 @@ async def done():
         print(f"Error: {e}")
 
 async def main():
+    await bot.start()
+    for x in ALL_SETTINGS:
+        imported_module = importlib.import_module("Magic.assistant." + x)
+        importlib.reload(imported_module)
+    await ubot.start()
+    for xx in ALL_MODULES:
+        imported_module = importlib.import_module("Magic.plugins." + xx)
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            imported_module.__MODULE__ = imported_module.__MODULE__
+        if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
+            if not imported_module.__MODULE__.lower() in HELP_COMMANDS:
+                HELP_COMMANDS[imported_module.__MODULE__.lower()] = imported_module
+            else:
+                raise Exception("Terdeteksi plugins yanh sama .")
+        if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+            HELP_COMMANDS[imported_module.__MODULE__.lower()] = imported_module
+    ubot_mod = ""
+    bot_mod = ""
+    j = 1
+    for i in ALL_MODULES:
+        if j == 4:
+            ubot_mod += "|{:<15}|\n".format(i)
+            j = 0
+        else:
+            ubot_mod += "|{:<15}".format(i)
+        j += 1
+    j = 1
+    for i in ALL_SETTINGS:
+        if j == 4:
+            bot_mod += "|{:<15}|\n".format(i)
+            j = 0
+        else:
+            bot_mod += "|{:<15}".format(i)
+        j += 1
+    print("+===============================================================+")
+    print("|                      Userbot Modules                          |")
+    print("+===============+===============+===============+===============+")
+    print(ubot_mod)
+    print("+===============+===============+===============+===============+\n")
+    print("+===============================================================+")
+    print("|                    Assistant Modules                          |")
+    print("+===============+===============+===============+===============+")
+    print(bot_ubot)
+    print("+===============+===============+===============+===============+")
+    print("Berhasil Menginstall Plugins")
     try:
         print(f"Koneksi Ke Database {MDB.name}...")
         if MDB.ping():
             print(f"Koneksi Berhasil Ke {MDB.name}..")
-        if bot:
-            await bot.start()
-            bot_id = (await bot.get_me()).username
-            print(f"Bot ID: {bot_id} Berhasil Diaktifkan.")
-            
-        if ubot:
-            await ubot.start()
-            client_id = (await ubot.get_me()).id
+            xxx = (await ubot.get_me()).id
             MDB.set_key("OWNER_ID", ubot.me.id)
-            print(f"Client ID: {client_id} Berhasil Diaktifkan")
-        
-        await loadPlugins()
         await done()
         await idle()
         await aiosession.close()
