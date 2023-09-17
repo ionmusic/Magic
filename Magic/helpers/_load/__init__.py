737941d376f237f71a83441db023e0e11060b045
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-09-17 15:43:30 +0700

    update __init__.py

--- Magic/helpers/_load/__init__.py
+++ Magic/helpers/_load/__init__.py
@@ -8,25 +8,11 @@ def where_hosted():
     
 HOSTED_ON = where_hosted()
 
-
 logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.ERROR)
 
 formatter = logging.Formatter("[%(levelname)s] - %(name)s - %(message)s", "%d-%b %H:%M")
 stream_handler = logging.StreamHandler()
 
 stream_handler.setFormatter(formatter)
-connection_handler = ConnectionHandler()
-
 logger.addHandler(stream_handler)
-logger.addHandler(connection_handler)
-
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-logging.getLogger("pyrogram.client").setLevel(logging.WARNING)
-logging.getLogger("pyrogram.session.auth").setLevel(logging.CRITICAL)
-logging.getLogger("pyrogram.session.session").setLevel(logging.CRITICAL)
-
-LOGS = logging.getLogger(__name__)
-
-def LOGGER(name: str) -> logging.Logger:
-    return logging.getLogger(name)
