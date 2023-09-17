10995190e9832b07a07f6156dddf6cf9f2c5195e
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-09-17 15:35:20 +0700

    update __init__.py

--- Magic/helpers/_load/__init__.py
+++ Magic/helpers/_load/__init__.py
@@ -16,6 +16,17 @@ formatter = logging.Formatter("[%(levelname)s] - %(name)s - %(message)s", "%d-%b
 stream_handler = logging.StreamHandler()
 
 stream_handler.setFormatter(formatter)
+connection_handler = ConnectionHandler()
+
+logger.addHandler(stream_handler)
+logger.addHandler(connection_handler)
+
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("pyrogram.client").setLevel(logging.WARNING)
+logging.getLogger("pyrogram.session.auth").setLevel(logging.CRITICAL)
+logging.getLogger("pyrogram.session.session").setLevel(logging.CRITICAL)
+
+LOGS = logging.getLogger(__name__)
 
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
