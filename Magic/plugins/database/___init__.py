3c37e355a8f98f0b43271ca4853fe9c5ed521272
IONBOT <113283570+ionmusic@users.noreply.github.com>
2023-09-19 00:23:45 +0700

    Update ___init__.py

--- Magic/plugins/database/___init__.py
+++ Magic/plugins/database/___init__.py
@@ -1,3 +1,9 @@
+import motor.motor_asyncio
 from Magic.helpers._database import MDB
 
 from .dbgcast import *
+
+from config import MONGO_URL
+cli = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
+
+db = cli.program
