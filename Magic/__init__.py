9499184eebad70169145ce912c99bd8daf42b632
IONBOT <113283570+ionmusic@users.noreply.github.com>
2023-09-19 01:34:15 +0700

    Update __init__.py

--- Magic/__init__.py
+++ Magic/__init__.py
@@ -1,4 +1,4 @@
-
+import logging
 import asyncio
 
 from aiohttp import ClientSession
@@ -10,6 +10,20 @@ aiosession = ClientSession()
 loop = asyncio.get_event_loop_policy()
 event_loop = loop.get_event_loop()
 
+FORMAT = "[Magic] %(message)s"
+logging.basicConfig(
+    handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
+    level=logging.INFO,
+    format=FORMAT,
+    datefmt="[%X]",
+)
+logging.getLogger("pyrogram").setLevel(logging.INFO)
+logging.getLogger("pymongo", "redis").setLevel(logging.WARNING)
+
+LOGGER = logging.getLogger('[Magic]')
+LOGGER.info("Ubot is starting. | An Magic Project Parts. | Licensed under GPLv3.")
+LOGGER.info("Not affiliated to other anime or Villain in any way whatsoever.")
+LOGGER.info("Project maintained by: github.com/Team-Pesulap (t.me/pesulaptelegram)")
 
 bot = MPBot()
 
